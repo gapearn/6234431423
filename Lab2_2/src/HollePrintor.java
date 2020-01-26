af50b25c445c8fe32230e0376db9af19594b01df
gapearn <59549963+gapearn@users.noreply.github.com>
2020-01-26 21:19:37 +0700

    Add files via upload

---
+++ Lab2_2/src/HollePrintor.java
@@ -0,0 +1,11 @@
+
+public class HollePrintor {
+    public static void main(String[] args) {
+    String text1 = "Hello, World!";
+    String text2 = text1.replace("o","E");
+    String text3 = text2.replace("e","o");
+    String text4 = text3.replace("E","e");
+    System.out.println(text4);
+    }
+    
+}
