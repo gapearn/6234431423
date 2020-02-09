537938c25fe3da5229827c41c42a69b498b2c049
gapearn <59549963+gapearn@users.noreply.github.com>
2020-02-09 23:51:45 +0700

    Add files via upload

---
+++ lab4_3/src/lab4_3/TimeInterval.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_3;
+
+/**
+ *
+ * @author thamolwan
+ */
+public class TimeInterval 
+{
+    private int start;
+    private int end;
+    
+    public TimeInterval(int start,int end)
+    {
+        this.start=start;
+        this.end=end;
+    }
+    public int getHours()
+    {
+        double minStart = Math.floor((start/100)*60) + (start%100);
+        double minEnd = Math.floor((end/100)*60) + (end%100);
+        double hours = Math.floor(minEnd - minStart)/60;
+        return (int)hours;
+    }
+    public int getMinutes()
+    {
+        double minStart = Math.floor((start/100)*60) + (start%100);
+        double minEnd = Math.floor((end/100)*60) + (end%100);
+        double min = (minEnd - minStart)%60;
+        return (int)min;
+    }
+}
